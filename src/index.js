a798a573637d982cccc579a430cfad40665ae679
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-05-16 14:47:35 -0400

    pwa instalado

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -9,4 +10,15 @@ root.render(
     <App />
   </StrictMode>
 );
-serviceWorker.register();
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
